8f0bb2ba2528c5bb1d03d3a3fa7a1e2649893c7c
nonadminus <ignatii1994-23-10.ru@mail.ru>
2021-05-12 16:09:01 +0700

    test

--- authenticate.js
+++ authenticate.js
@@ -1,6 +1,6 @@
 function authenticate(login, password) {
 	if (login === 'login' && password === 'password') {
-		return "Welcome!";
+		return "Welcome!s";
 	} else {
 		return "Login is incorrect!";
 	}
