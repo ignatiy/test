116ebe25be8002a76f758ea70b049d9e1129aa60
nonadminus <ignatii1994-23-10.ru@mail.ru>
2021-05-12 11:55:50 +0700

    changed auth function

--- auth.js
+++ auth.js
@@ -1,7 +1,7 @@
-function auth(login, password) {
-	if (login === 'login' && password === 'password') {
-		return "Is correct!"
+function auth(login) {
+	if (login === 'login') {
+		return "Login is correct!"
 	} else {
-		return "Incorrect!"
+		return "Login incorrect!"
 	}
 }
